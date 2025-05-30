98521d346fc31df9432a5b36ce2e5cfdc317cd0a
Suchismita Sahu <95347254+Suchi-BITS@users.noreply.github.com>
2025-05-30 20:05:01 +0530

    Update my_agent.py

--- agent/my_agent.py
+++ agent/my_agent.py
@@ -1,6 +1,6 @@
 # my_agent/agent.py
 
-from finops_prompts import load_prompt
+from finops_prompts.prompt_loader import load_prompt
 
 query = "Give me EC2 cost trend for last month"
 prompt_template = load_prompt("cot_structuring")
